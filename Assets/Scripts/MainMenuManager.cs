97971e0726be33023eff6b037fd66c09e7e4c5e6
AsclepiiusUnknown <55474351+AsclepiiusUnknown@users.noreply.github.com>
2020-06-28 20:34:04 +1000

    Started Options UI

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    #region Game Management
+    public void NewGame(string gameScene)
+    {
+        ChangeScene(gameScene);
+    }
+
+    public void LoadGame()
+    {
+
+    }
+    #endregion
+
+    public void QuitApplication()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
